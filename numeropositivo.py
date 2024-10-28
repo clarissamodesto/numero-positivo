bd521507c0ab79feda8ac356e3418f722335cee7
Clarissa Cruz <141753467+clarissamodesto@users.noreply.github.com>
2024-10-28 08:05:24 -0300

    Add files via upload

---
+++ numeropositivo.py
@@ -0,0 +1,15 @@
+'''
+nome: Clarissa Cruz
+data: 28/10/2024
+'''
+
+#Faça um programa que peça um valor e mostre na tela se o valor é positivo ou negativo
+
+valor = float (input('Digite um número: '))
+
+if valor > 0:
+    print('Esse número é positivo')
+elif valor == 0:
+    print ('Esse número é zero')
+else:
+    print('Esse número é negativo')
